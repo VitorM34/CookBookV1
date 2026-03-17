910a4e8ff37cdb63d40d4321cab40afbb047f13d
VitorM34 <VitorM34@users.noreply.github.com>
2026-03-17 19:11:55 -0300

    Implement register user use case

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,6 +1,34 @@
+using MyRecipeBook.Communication.Requests;
+
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
 public class RegisterUserUseCase
 {
-    
+    public ResponseRegisterUserJson Execute(RequestRegisterUserJson request)
+    {
+        Validate(request);
+        
+        //mapear a request em uma entidade 
+        //criptografia da senha 
+        //salvar no banco 
+        
+        
+        return new ResponseRegisterUserJson
+        {
+            Name =  request.Name,
+        };
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+         var result = validator.Validate(request);
+
+         if (result.IsValid == false)
+         {
+             var errorsMessages = result.Errors.Select(e => e.ErrorMessage);
+             throw new Exception();
+         }
+    }
 }
--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -1,6 +1,14 @@
+using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
-public class RegisterUserValidator
+public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>    
 {
-    
+    public RegisterUserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty();
+        RuleFor(user => user.Email).NotEmpty();
+        RuleFor(user => user.Email).EmailAddress();
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
 }
