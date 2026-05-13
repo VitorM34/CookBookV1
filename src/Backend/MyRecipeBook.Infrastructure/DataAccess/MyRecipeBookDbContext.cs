c28efc46a81e902f4e7187f65c1d8b8ae38cf565
VitorM34 <VitorM34@users.noreply.github.com>
2026-05-12 22:09:59 -0300

    add configuration of ORM - Entity Framework Core

--- src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace MyRecipeBook.Infrastructure.DataAccess;
 
-public class MyRecipeBookDbContext
+public class MyRecipeBookDbContext : DbContext
 {
+    public MyRecipeBookDbContext(DbContextOptions options) : base(options) { }
     
-}
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder);
+    
+    }
+
